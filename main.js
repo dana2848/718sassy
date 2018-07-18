035feec8b774756ac79071e7691b2ffe745458a6
YOUR NAME <YOUR@EMAIL.com>
2018-07-18 18:06:37 -0400

    js

---
+++ main.js
@@ -0,0 +1,48 @@
+
+count = 0;
+console.log('javascript is enabled');
+
+form = document.getElementById('signup');
+console.log(form);
+
+users = [];
+totalUsers=0;
+function createUser(name, email) {
+  user = {
+    name: name,
+    email: email
+  }
+
+user.push(user);
+totalUsers++;
+count++;
+
+class user {
+  constructor(name, email) {
+
+    this.name = name;
+    this.email = email;
+  }
+}
+class Profile {
+  constructor(url, location) {
+    this.url=url;
+    this.location=location;
+  }
+
+}
+}
+
+greeting =()=> {alert('welcome to the website')};
+goodbye =()=> {alert('good bye class')};
+countThis = count => {alert('there are' + count)};
+
+var number = prompt('enter a number');
+function double(x) {
+
+result = x * 2;
+  console.log(result);
+}
+
+double(x);
+console.log('there are currently' + count + ' users')
